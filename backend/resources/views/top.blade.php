51ba32942ebde398ebf3332adb72f613c41dba8f
Shimizu Kento <aibo396@yahoo.co.jp>
2020-11-01 16:24:28 +0900

    test changing word

--- backend/resources/views/top.blade.php
+++ backend/resources/views/top.blade.php
@@ -13,7 +13,7 @@
   <div class="text-center">
     <img src="{{ asset('image/topWelcome.jpg') }}" style="width: 85%;" alt="top image">
   </div>
-  <h3 class="suggest-msg text-center">帰国時の予定を建てましょう。</h3>
+  <h3 class="suggest-msg text-center">帰国時の予定を建てましょう</h3>
   <div class="main-tag-group">
     <button class="btn main-tag btn-primary btn-lg" onclick="clickTag1()">#家族と会う</button>
     <button class="btn main-tag btn-primary btn-lg" onclick="clickTag2()">#買い物に行く</button>
