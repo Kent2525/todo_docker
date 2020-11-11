9322196f9a05a5b77b044b3d309ef8131e62f6db
Shimizu Kento <aibo396@yahoo.co.jp>
2020-11-11 19:02:10 +0900

    delete env setting for config.yml

--- backend/config/database.php
+++ backend/config/database.php
@@ -110,19 +110,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-        'circle_test' => [
-            'driver' => 'mysql',
-            'host' => '127.0.0.1',
-            'port' => '3306',
-            'database' => 'circle_test',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ]
     ],
 
     /*
