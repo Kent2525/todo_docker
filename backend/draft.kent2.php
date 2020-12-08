9563bf652c59cc95347cda3d1fd72b462a46f9db
Shimizu Kento <aibo396@yahoo.co.jp>
2020-12-08 15:25:04 +0900

    php review construct in kentdraft2

--- backend/draft.kent2.php
+++ backend/draft.kent2.php
@@ -1,7 +1,24 @@
-<!-- PHP復習 -->
+PHP復習
 
-<!-- ■ foreach -->
+■ foreach
 <?php foreach ($menus as $menu): ?>
   <h3><?php echo $menu->name ?></h3>
 <?php endforeach ?>
-<!-- :を入れたり、endforeachの場所など注意が必要。 -->
+:を入れたり、endforeachの場所など注意が必要。
+
+■ construct
+class Man
+{
+	protected  $name;
+	function __construct()	{
+    <!-- 行いたい処理を書く。この場合、Man = 鈴木となる。-->
+		$this->name = "鈴木";
+	}
+	function show() {
+		echo $this->name;
+	}
+}
+
+$seito = new Man();
+$seito->show();
+<!-- showの結果、鈴木が出力される。 -->
