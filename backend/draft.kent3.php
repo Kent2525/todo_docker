86b35b46872a84560277886e58ff480ffd196845
Shimizu Kento <aibo396@yahoo.co.jp>
2021-01-20 19:35:58 +0900

    created draftkent3 for anysence working

---
+++ backend/draft.kent3.php
@@ -0,0 +1,10 @@
+１月20日
+■ EC CUBE
+EC CUBEでJavascriptで顧客のクレジット情報などの情報をjsでサービス会社のサーバーに情報を投げて、専用トークンを取得する。
+アクセスURLを変更に伴って、アクセスパスを変更する業務
+
+■ OpenStreetMapで動いてしまう画像を静止画像にする。
+OpenStreetMap + Leafletを使用しているがうまくいかない。
+
+■ Toggl と backlogの設定
+別メモ参照
