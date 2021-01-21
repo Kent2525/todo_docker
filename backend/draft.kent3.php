8174b2b75bfd09dfa181c66b739924410deb944d
Shimizu Kento <aibo396@yahoo.co.jp>
2021-01-21 17:34:36 +0900

    review for 1/20 in draft.kent3.php

--- backend/draft.kent3.php
+++ backend/draft.kent3.php
@@ -8,3 +8,15 @@ OpenStreetMap + Leafletを使用しているがうまくいかない。
 
 ■ Toggl と backlogの設定
 別メモ参照
+
+1月21日
+■ gitclone
+backlogからgit cloneを行う。https通信で行う。
+再pushの際にremote先変更後エラーが発生。ググって解決
+
+■ OpenStreetMap + Leaflet
+実際のローカル環境で実装。スタンダートな物は実装する事ができた。
+
+■ EC CUBE
+トークン先変更部分は見つける事はできたが、どういう仕組みで動いているのかわからない。
+特にcontrollerの部分がどこでクラスが処理されているのかわからない。
